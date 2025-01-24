bcbbd8eff4e940562c27cd00a6efe6e62e578984
G1ovanaRD <a33998@alumnos.uaslp.mx>
2025-01-24 00:28:01 -0600

    Gameover  y Win(aun falta editar el fondo de win)

---
+++ scripts/gameover.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+func _on_volver_pressed() -> void:
+	_on_start_button_pressed()
