7c30dd5f99ae1b6a55b9efa124c59c1ff878c270
G1ovanaRD <a33998@alumnos.uaslp.mx>
2025-01-23 22:32:19 -0600

    no puedo cambiar de pantalla entre el menu

---
+++ scripts/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_start_button_pressed():
+	get_tree().change_scene("res://scenes/level_1.tscn")
+
+func _on_button_pressed() -> void:
+	print("Intentando cambiar de escena...")
+	print(get_tree())  # Esto debería mostrar: [SceneTree]
+	_on_start_button_pressed()
