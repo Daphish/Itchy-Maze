afc3828179df2519fdc988a809e0a2f062a02906
Daphish <ivan.davila.2003@gmail.com>
2025-01-23 22:46:46 -0600

    Menu play button bug fixed

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_start_button_pressed():
-	get_tree().change_scene("res://scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 func _on_button_pressed() -> void:
 	print("Intentando cambiar de escena...")
